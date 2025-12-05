8a20d91c22751e646f2969ffa52de6f395194d77
Bastien Gorissen <bastien@teampanoptes.be>
2025-12-05 14:24:01 +0100

    Fix hit points

--- Assets/#Project/Scripts/HitPointsCondition.cs
+++ Assets/#Project/Scripts/HitPointsCondition.cs
@@ -4,19 +4,22 @@ using Unity.Behavior;
 using UnityEngine;
 
 [Serializable, Unity.Properties.GeneratePropertyBag]
-[Condition(name: "Hit Points", story: "[Destructable] HP less than [Value]", category: "Conditions/Boss Fight", id: "8929f2b25b2666fbd2ee95caa68cfda0")]
+[Condition(name: "Hit Points", story: "[Agent] HP less than [Value]", category: "Conditions/Boss Fight", id: "8929f2b25b2666fbd2ee95caa68cfda0")]
 public partial class HitPointsCondition : Condition
 {
-    [SerializeReference] public BlackboardVariable<Destructable> Destructable;
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
     [SerializeReference] public BlackboardVariable<int> Value;
+
+    private Destructable destructable;
 
     public override bool IsTrue()
     {
-        return Destructable.Value.CurrentHealth <= Value.Value;
+        return destructable.CurrentHealth <= Value.Value;
     }
 
     public override void OnStart()
     {
+        destructable = Agent.Value.GetComponentInChildren<Destructable>();
     }
 
     public override void OnEnd()
